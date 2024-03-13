f5586c032a3acef041778669e115d447b416bd23
ajo <115482909+ajoborgvold@users.noreply.github.com>
2024-03-13 11:36:32 +0100

    delete note

--- ajos-notes/df-essentials.py
+++ ajos-notes/df-essentials.py
@@ -1,12 +1,13 @@
 import pandas as pd
 
-df = pd.read_csv("tøjfirma_sales_data.csv")
+df = pd.read_csv("../tøjfirma_sales_data.csv")
 
 #==================#
 # Get the first x rows; x in square brackets
 
 first_10_rows = df.head(10)
 # print(first_10_rows)
+
 #==================#
 
 
@@ -40,7 +41,40 @@ selected_rows = df[1:5]
 #==================#
 # Iterate through columns
 
-for index, row in df[2:5].iterrows():
-    print(f"Index: {index}. Product: {row['Product']}. Category: {row['Category']}.")
+# for index, row in df[2:5].iterrows():
+#     print(f"Index: {index}. Product: {row['Product']}. Category: {row['Category']}.")
+
+#==================#
+
+
+#==================#
+# Desribe the data
+
+describe_data = df.describe()
+print(describe_data)
+
+data_info = df.info()
+print(data_info)
+
+# Get info about non-numeric values
+# describe_include = df.describe(include=["object", "category"])
+# print(describe_include)
+
+#==================#
+
+
+#==================#
+# Calculate sales
+
+# Assuming that the sale price is for all units sold
+mean_without_units = df["Sale Price"].mean()
+# print("Mean, without units count", mean_without_units)
+
+# Assuming that the sale price is per unit
+product_sales_price = df["Sale Price"] * df["Units Sold"]
+mean_with_units = product_sales_price.mean()
+# print("Mean, with units count", mean_with_units)
+
+
 
 #==================#
