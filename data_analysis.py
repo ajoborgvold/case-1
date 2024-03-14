d49052f2553d565f7cf1d1f386ffb399d1f3bc65
ajo <115482909+ajoborgvold@users.noreply.github.com>
2024-03-14 16:54:37 +0100

    WIP: data analysis and visualization

--- data_analysis.py
+++ data_analysis.py
@@ -13,11 +13,23 @@ average_sale_per_row = sale_per_row.mean()
 
 
 #=================================#
-# Identify top 5 products:
-top_5_by_sale = df.loc[sale_per_row.nlargest(5).index]
-top_5_by_quantity = df.loc[df["Units Sold"].nlargest(5).index]
-# print(top_5_by_sale)
-# print(top_5_by_quantity)
+# Sale per product; identify top 5 products
+products = df.groupby("Category")["Product"].apply(lambda x: x.unique())
+product_sales = df.groupby(["Category", "Product"]).apply(lambda x: (x["Units Sold"] * x["Sale Price"]).sum(), include_groups=False)
+sorted_product_sales = product_sales.sort_values(ascending=False)
+top_5_products_sale = sorted_product_sales.head(5)
+bottom_5_products_sale = sorted_product_sales.tail(5)
+# print(sorted_product_sales)
+# print(top_5_products_sale)
+# print(bottom_5_products_sale)
+
+product_units_sold = df.groupby(["Category", "Product"])["Units Sold"].sum()
+sorted_product_units_sold = product_units_sold.sort_values(ascending=False)
+top_5_products_units = sorted_product_units_sold.head(5)
+bottom_5_products_units = sorted_product_units_sold.tail(5)
+# print(top_5_products_units)
+# print(bottom_5_products_units)
+
 #=================================#
 
 
@@ -34,12 +46,48 @@ count_products_men = (df["Category"] == "Men").sum()
 # Count units sold in the three categories
 units_sold_per_category = df.groupby("Category")["Units Sold"].sum()
 # print(units_sold_per_category)
+total_sale_per_category = sale_per_row.groupby(df["Category"]).sum()
+# print(total_sale_per_category)
 #=================================#
 
 
 #=================================#
 # Seasonal trends in sale
 datetime = pd.to_datetime(df["Sale Date"])
-print(datetime)
+
+# Sale trends per month; total sale and units
+total_sale_per_month = sale_per_row.groupby(datetime.dt.month_name()).sum()
+units_sold_per_month = df["Units Sold"].groupby(datetime.dt.month_name()).sum()
+# print(total_sale_per_month)
+# print(units_sold_per_month)
+
+# Sale trends per day; total sale and units
+total_sale_per_day = sale_per_row.groupby(datetime.dt.date).sum()
+units_sold_per_day = df["Units Sold"].groupby(datetime.dt.date).sum()
+# print(total_sale_per_day)
+# print(units_sold_per_day)
+
+# Sale trends per month per category; total sale and units
+total_sale_per_category_per_month = sale_per_row.groupby([df["Category"], datetime.dt.month]).sum()
+units_sold_per_category_per_month = df.groupby([df["Category"], datetime.dt.month])["Units Sold"].sum()
+# print(total_sale_per_category_per_month)
+# print(units_sold_per_category_per_month)
+
+# Sale trends per day per category; total sale and units
+total_sale_per_category_per_day = sale_per_row.groupby([df["Category"], datetime.dt.date]).sum()
+units_sold_per_category_per_day = df.groupby([df["Category"], datetime.dt.date])["Units Sold"].sum()
+# print(total_sale_per_category_per_day)
+# print(units_sold_per_category_per_day)
+
+# Days with the highest/lowest sale, day average and median sale
+sorted_total_sale_per_day = total_sale_per_day.sort_values(ascending=False)
+top_day = sorted_total_sale_per_day.head(1)
+bottom_day = sorted_total_sale_per_day.tail(1)
+day_average = total_sale / len(total_sale_per_day)
+median_sale = total_sale_per_day.median()
+# print(top_day)
+# print(bottom_day)
+# print(f"Average sale per day: {day_average}")
+# print(f"Median sale: {median_sale}")
 
 #=================================#
---
+++ data_visualization.py
@@ -0,0 +1,66 @@
+import matplotlib.pyplot as plt
+from matplotlib.dates import DateFormatter
+import numpy as np
+
+from data_analysis import total_sale_per_day, units_sold_per_day, total_sale_per_category, product_sales
+
+def show_sales_per_day():
+    dates = total_sale_per_day.index
+    sales = total_sale_per_day.values
+    units = units_sold_per_day.values
+
+    fig, ax = plt.subplots(2, 1)
+
+    ax[0].plot(dates, sales, linewidth=2.0)
+    ax[0].set_xlabel("Date")
+    ax[0].set_ylabel("Total sale")
+    ax[0].set_title("Total sales per day")
+    ax[0].tick_params(axis='x', rotation=45)
+    ax[0].xaxis.set_major_formatter(DateFormatter("%d-%m"))
+
+    ax[1].plot(dates, units, linewidth=2.0)
+    ax[1].set_xlabel("Date")
+    ax[1].set_ylabel("Units sold")
+    ax[1].set_title("Total units sold per day")
+    ax[1].tick_params(axis='x', rotation=45)
+    ax[1].xaxis.set_major_formatter(DateFormatter("%d-%m"))
+    
+    
+    plt.tight_layout()
+    plt.show()
+
+# show_sales_per_day()
+
+
+
+
+
+def show_sale_per_category():
+    ax = plt.subplots()
+    
+    categories = total_sale_per_category.index
+    values = total_sale_per_category.values
+    colors = ['#eedd82', '#ff6666', '#90ee90']
+    ax.pie(values, labels=categories, colors=colors, autopct='%1.1f%%', startangle=140)
+    ax.set_title("Total sales distribution by category")
+    
+    plt.tight_layout()
+    plt.show()
+
+# show_sale_per_category()
+
+
+def show_products_categories():
+    ax = plt.subplots()
+
+    product_sales_pivot = product_sales.unstack()
+    product_sales_pivot.T.plot(kind='bar', ax=ax, width=0.8)
+    ax.set_xlabel('Product')
+    ax.set_ylabel('Total Sales')
+    ax.set_title('Total Sales for each product with category differentiation')
+    plt.xticks(rotation=45, ha='right')
+
+    plt.tight_layout()
+    plt.show()
+
+show_products_categories()
